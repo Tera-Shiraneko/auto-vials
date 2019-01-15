a69468309b22ae9a9fe85ffb4593c674ea99a3c0
Shalltearz <42479034+Tera-Shiraneko@users.noreply.github.com>
2019-01-15 22:31:35 +0100

    Add files via upload

---
+++ settings_structure.js
@@ -0,0 +1,17 @@
+module.exports = [
+    {
+        "key": "actiondelay",
+        "name": "Delay between different actions in milliseconds.",
+        "type": "number"
+    },
+    {
+        "key": "charselectdelay",
+        "name": "Delay of selecting the next character in milliseconds.",
+        "type": "number"
+    },
+    {
+        "key": "elinuid",
+        "name": "Here you can customize the item that should be used.",
+        "type": "number"
+    }
+];
